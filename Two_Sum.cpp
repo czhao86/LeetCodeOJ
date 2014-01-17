014ec021fdcd42f1b5d58b30f90ce25e7b6d1de9
czhao86 <czhao86@gmail.com>
2014-01-17 15:51:07 -0500

    Update Two_Sum.cpp

--- Two_Sum.cpp
+++ Two_Sum.cpp
@@ -1,39 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution 
 {
 public:
-	vector<int> twoSum(vector<int> &numbers, int target)
+	vector<int> twoSum(vector<int> &numbers, int target) 
 	{
-		vector<int> result;
-		vector<int>::iterator it;
-		vector<int>::iterator it2;
-		for (it = numbers.begin(); it != numbers.end(); ++it)
+		vector<pair<int, int>> num;
+		for (int i = 0; i < numbers.size(); ++i)
 		{
-			for (it2 = it + 1; it2 != numbers.end(); ++it2)
-			{
-				if (*it + *it2 == target)
-				{
-					result.push_back(it - numbers.begin() + 1);
-					result.push_back(it2 - numbers.begin() + 1);
-					break;
-				}
-			}
+			num.push_back(make_pair(numbers[i],i+1));
 		}
-		return result;
+		sort(num.begin(),num.end());
+		int m, n;
+		int begin=0, end=numbers.size()-1;
+		while (begin < end)
+		{
+			if (num[begin].first + num[end].first == target)
+			{
+				m = min(num[begin].second, num[end].second);
+				n = max(num[begin].second, num[end].second);
+				break;
+			}
+			else if (num[begin].first + num[end].first > target)
+				end--;
+			else begin++;
+		}
+		vector<int> res;
+		res.push_back(m);
+		res.push_back(n);
+		return res;
 	}
 };
 
 int main()
 {
-	Solution sol;
-	int p[]= { 2, 7, 11, 15 };
-	vector<int> numbers(p, p + 4);
-	int target = 26;
-	vector<int> result=sol.twoSum(numbers, target);
-	cout << result.front() << endl;
-	cout << result.back() << endl;
 	return 0;
 }
